ec073356f03cfedee03aa4fc8a74ea22f0faca76
Joseph James Weitzel <joe@box.biz>
2019-01-30 11:24:47 -0500

    Create onMutation.jquery.js

---
+++ onMutation.jquery.js
@@ -0,0 +1,20 @@
+(function($){
+  $.fn.onMutation = function(fn, mutations){
+    var config = mutations || { attributes: true };
+    return $(this).each(function(index, targetNode){
+      if (typeof MutationObserver === typeof undefined || MutationObserver === null) return;
+      var callback = function(mutationsList, observer) {
+            for(var mutation of mutationsList) {
+              if (typeof config[mutation.type] !== typeof undefined && config[mutation.type] !== null) {
+                switch(mutation.type) {
+                  case 'childList' : fn.call(targetNode, mutation); break;
+                  case 'attributes' : fn.call(targetNode, mutation, mutation.attributeName); break;
+                }
+              } 
+            }
+          },
+          observer = new MutationObserver(callback);
+      observer.observe(targetNode, config);
+    });
+  }
+}(jQuery));
